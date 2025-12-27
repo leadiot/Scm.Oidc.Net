b58bec688f61e0d69ed9380cc3c89ac18096bf8c
Aven <avenyao@outlook.com>
2025-12-27 08:46:33 +0800

    v1.1.3版本发布。

--- Scm.Oidc.Client/OidcClient.cs
+++ Scm.Oidc.Client/OidcClient.cs
@@ -60,7 +60,7 @@ namespace Com.Scm.Oidc
         /// <summary>
         /// 类库版本
         /// </summary>
-        public const string Ver = "1.1.2";
+        public const string Ver = "1.1.3";
 
         /// <summary>
         /// OIDC配置
